8e11f448085d9ed2fcdd3f93c80c2f187815984b
Manassarn Manoonchai <manassarn@gmail.com>
2023-03-12 17:02:28 +0700

    Batch extract script

--- extractor/batch.js
+++ extractor/batch.js
@@ -1,4 +1,7 @@
+const PDFExtract = require("pdf.js-extract").PDFExtract
+const axios = require("axios")
 const csv = require("csv-parser")
+const matter = require("gray-matter")
 const fs = require("fs")
 const path = require("path")
 
@@ -7,45 +10,89 @@ const filterCriterias = ["ก"]
 
 const results = []
 
+const force = process.argv.includes("--force")
+
 fs.createReadStream(filePath)
   .pipe(csv())
   .on("data", (row) => {
     if (filterCriterias.includes(row.ประเภท)) {
-      results.push(row)
+      if (results.length < 5) {
+        // Limit to 5 for testing
+        results.push(row)
+      }
     }
   })
-  .on("end", () => {
+  .on("end", async () => {
     // console.log(results)
 
-    results.forEach(
-      ({
-        วันที่: date,
-        เรื่อง: name,
-        เล่ม: volume,
-        เล่มที่: sub_volume,
-        ตอน: section,
-        ประเภท: category,
-        URL: url,
-      }) => {
-        // Skip if file is already exist
-        const filename = path.basename(url, ".pdf")
+    await Promise.all(
+      results.map(async (entry) => {
+        const filename = path.basename(entry.URL, ".pdf")
 
-        if (fs.existsSync(`../data/pdf/${filename}.pdf`)) {
-          console.log(`${filename}.pdf already exist`)
+        if (fs.existsSync(`../entries/${filename}.md`) && !force) {
+          console.log(`${filename}.md already exist`)
+          console.log("Use --force to override the files")
         } else {
-          // TODO: Create new file from TEMPLATE.md
-          // const template = fs.readFileSync("../data/TEMPLATE.md", "utf8")
-
-          console.log({
-            date,
-            name,
-            volume,
-            sub_volume,
-            section,
-            category,
-            url,
-          })
+          return await downloadAndExtract(filename, entry)
         }
-      }
+      })
     )
   })
+
+async function downloadAndExtract(filename, entry) {
+  const {
+    วันที่: date,
+    เรื่อง: name,
+    เล่ม: volume,
+    หน้า: page,
+    เล่มที่: subPage,
+    ตอน: section,
+    ประเภท: category,
+    URL: url,
+  } = entry
+
+  const template = fs.readFileSync("../TEMPLATE.md", "utf8")
+
+  const { data: templateData, content: _newContent } = matter(template)
+
+  const data = Object.assign({}, templateData)
+
+  data.name = name
+  data.date = new Date(date).toISOString().split("T")[0]
+  data.category = category
+  data.volume = +volume
+  data.section = +section
+  data.page = subPage ? `${page} ${subPage}` : +page
+  data.source = url
+
+  const pdfExtract = new PDFExtract()
+  const options = {} /* see below */
+
+  const response = await axios.get(url, { responseType: "arraybuffer" })
+  const buffer = Buffer.from(response.data)
+
+  const pdfData = await pdfExtract.extractBuffer(buffer, options)
+
+  const content = [
+    `\n# ${name}`,
+    ...pdfData.pages.map((page) =>
+      page.content
+        .map((item) => item.str)
+        .join(" ")
+        .replace(/[ ]า/g, "ำ")
+        .replace(/\s+/g, " ")
+    ),
+  ].join("\n\n")
+
+  const newFrontmatter = matter.stringify(content, data)
+
+  fs.writeFileSync(`../entries/${filename}.md`, unsarabun(newFrontmatter))
+
+  console.log(`${filename}.md created`)
+}
+
+function unsarabun(content) {
+  return content.replace(/[๐-๙]/g, (match) => {
+    return match.charCodeAt(0) - 3664
+  })
+}
